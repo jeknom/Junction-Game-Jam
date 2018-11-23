56da955e8697e59ef47ad6b28e6e322fd8a26c94
mahad <36614535+tigereno@users.noreply.github.com>
2018-11-23 23:20:45 +0200

    simpleScore

---
+++ Junction/Assets/Scripts/GamePlayScript.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GamePlayScript : MonoBehaviour {
+
+    private bool canPress = false;
+
+    private int ggCount = 0;
+    private int bgCount = 0;
+
+    private float timeLeft = int.MaxValue;
+
+    [SerializeField] private GameObject[] toActivateGOs;
+
+    [SerializeField] private Text goodScoreText;
+    [SerializeField] private Text badScoreText;
+
+    void Start () {
+        Invoke("startGame",2f);
+	}
+	
+    void Update () {
+        if (canPress)
+        {
+            countDown();
+            detectKeyPress();
+        }
+    }
+
+    private void startGame()
+    {
+        toggleActiveState();
+        canPress = true;
+    }
+
+    private void detectKeyPress()
+    {
+        
+            if (Input.GetKeyUp(KeyCode.A))
+            {
+                ggCount++;
+            }
+
+            if (Input.GetKeyUp(KeyCode.L))
+            {
+                bgCount++;
+            }
+        updateScore();
+    }
+
+    private void countDown()
+    {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0)
+        {
+            gameOver();
+        }
+    }
+
+    private void gameOver()
+    {
+        canPress = false;
+        toggleActiveState();
+        updateScore();
+    }
+
+    private void toggleActiveState()
+    {
+        foreach (GameObject obj in toActivateGOs)
+        {
+            obj.SetActive(!obj.activeInHierarchy);
+        }
+    }
+
+    private void updateScore()
+    {
+        goodScoreText.text = "Good: " + ggCount;
+        badScoreText.text = "Bad: " + bgCount;
+    }
+}
